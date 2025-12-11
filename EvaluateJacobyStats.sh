92f82bf1be87651f54560c12d12459dd36980d6b
Your Name <you@example.com>
2025-12-11 12:52:47 +0100

    README Figs Shell scripts

--- EvaluateJacobyStats.sh
+++ EvaluateJacobyStats.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
 RootDir=$(dirname "$0" | xargs -i realpath {})
-if [ $# -gt 0 ]
- then
- BASEDIR=$(realpath "$1")
- else
- BASEDIR=$RootDir
+if [ $# -gt 0 ]; then
+  BASEDIR=$(realpath "$1")
+else
+  BASEDIR=$RootDir
 fi
 
-if [ $# -gt 1 ]
- then
- folderForDeffields=$2
- else
- exit 1
+if [ $# -gt 1 ]; then
+  folderForDeffields=$2
+else
+  exit 1
 fi
 
+#gtvs=$(find $BASEDIR -maxdepth 3 -type f -name 'GTV*.nrrd') # Frbg
+#gtvs=$(find $BASEDIR -maxdepth 3 -type f -name 'HM*res.nrrd') # TUM
+gtvs=$(find $BASEDIR -type d -name 'NRRD' -exec find {} -maxdepth 1 -type f -name 'HM*res.nrrd' \;)
+for gtv in $gtvs; do
+  imgDir=$(dirname $gtv)
+  filename=$(basename $gtv)
+  echo $gtv
 
-gtvs=$(find $BASEDIR -maxdepth 3 -type f -name 'GTV*.nrrd')
-for gtv in $gtvs
-do
-    imgDir=$(dirname $gtv)
-    filename=$(basename $gtv)
-    echo $gtv
-    
-    if [ ! -f "${imgDir}/BHFI_vtkMRMLLinearTransformNodeH12Dof.h5" ]; then
-    	#echo "rigid registration file does not exist"
-    	continue
-    fi
-    
-    if [ ! -f "${imgDir}/${folderForDeffields}/DefField.mha" ]; then
-   	 #echo "deformable registration file does not exist"
-   	 continue 	
-    fi
-    
-    python code/JacobyStats.py -m "${gtv}" -r "${imgDir}/BHFI_vtkMRMLLinearTransformNodeH12Dof.h5" -d "${imgDir}/${folderForDeffields}/DefField.mha" -o "jacobiStats10mm${folderForDeffields}.csv" -n "/home/fechter/Bilder/Atlas/seg4_short.nrrd" -c "10.0"
-    
-    echo "done"
+  #affineRegName="BHFI_vtkMRMLLinearTransformNodeH12Dof.h5" # Frbg
+  affineRegName="BHFI_vtkMRMLLinearTransformNodeH12DofContourBased.txt" # TUM
+
+  if [ ! -f "${imgDir}/${affineRegName}" ]; then
+    echo "rigid registration file ${imgDir}/${affineRegName}  does not exist"
+    continue
+  fi
+
+  if [ ! -f "${imgDir}/${folderForDeffields}/DefField.mha" ]; then
+    echo "deformable registration file does not exist"
+    continue
+  fi
+
+  python code/JacobyStats.py -m "${gtv}" -r "${imgDir}/${affineRegName}" -d "${imgDir}/${folderForDeffields}/DefField.mha" -o "jacobiStats10mm${folderForDeffields}TUM_tmp.csv" -n "/home/fechter/Bilder/Atlas/seg35_short.nrrd" -c "10.0"
+
+  echo "done"
 done
--- EvaluateTumorShrinkage.sh
+++ EvaluateTumorShrinkage.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
 RootDir=$(dirname "$0" | xargs -i realpath {})
-if [ $# -gt 0 ]
- then
- BASEDIR=$(realpath "$1")
- else
- BASEDIR=$RootDir
+if [ $# -gt 0 ]; then
+  BASEDIR=$(realpath "$1")
+else
+  BASEDIR=$RootDir
 fi
 
-if [ $# -gt 1 ]
- then
- folderForDeffields=$2
- else
- exit 1
+if [ $# -gt 1 ]; then
+  folderForDeffields=$2
+else
+  exit 1
 fi
 
+# gtvs=$(find $BASEDIR -maxdepth 3 -type f -name 'GTV*.nrrd') # Frbg
+gtvs=$(find $BASEDIR -type f -wholename '*NRRD/HM*res.nrrd') # TUM
+#gtvs=$(find $BASEDIR -type d -name 'NRRD' -exec find {} -maxdepth 1 -type f -name 'HM*res.nrrd' \;)
+for gtv in $gtvs; do
+  imgDir=$(dirname $gtv)
+  filename=$(basename $gtv)
+  echo $gtv
 
-gtvs=$(find $BASEDIR -maxdepth 3 -type f -name 'GTV*.nrrd')
-for gtv in $gtvs
-do
-    imgDir=$(dirname $gtv)
-    filename=$(basename $gtv)
-    echo $gtv
-    
-    if [ ! -f "${imgDir}/BHFI_vtkMRMLLinearTransformNodeH12Dof.h5" ]; then
-    	#echo "rigid registration file does not exist"
-    	continue
-    fi
-    
-    if [ ! -f "${imgDir}/${folderForDeffields}/DefField.mha" ]; then
-   	 #echo "deformable registration file does not exist"
-   	 continue 	
-    fi
-    python /home/fechter/workspace/AtlasRegistration/aladdin/code/ApplyTransformationsToFiles.py -i "${gtv}" -o "${imgDir}/${folderForDeffields}/${filename}" -r "${imgDir}/BHFI_vtkMRMLLinearTransformNodeH12Dof.h5" -d "${imgDir}/${folderForDeffields}/DefField.mha" -b
-    
-    name="${filename%.*}"
-    extension="${filename##*.}"
-    
-    python /home/fechter/workspace/AtlasRegistration/aladdin/code/ApplyTransformationsToFiles.py -i "${gtv}" -o "${imgDir}/${folderForDeffields}/${name}Rigid.${extension}" -r "${imgDir}/BHFI_vtkMRMLLinearTransformNodeH12Dof.h5" -b
-    
-    python gtvStats.py -f "${imgDir}/${folderForDeffields}/${name}Rigid.${extension}" -s "${imgDir}/${folderForDeffields}/${filename}" -o "gtvStats${folderForDeffields}.csv"
-    
-    echo "done"
+  # affineRegName="BHFI_vtkMRMLLinearTransformNodeH12Dof.h5" # Frbg
+  affineRegName="BHFI_vtkMRMLLinearTransformNodeH12DofContourBased.txt" # TUM
+
+  if [ ! -f "${imgDir}/${affineRegName}" ]; then
+    echo "rigid registration file does not exist"
+    continue
+  fi
+
+  if [ ! -f "${imgDir}/${folderForDeffields}/DefField.mha" ]; then
+    echo "deformable registration file does not exist"
+    continue
+  fi
+  python code/ApplyTransformationsToFiles.py -i "${gtv}" -o "${imgDir}/${folderForDeffields}/${filename}" -r "${imgDir}/${affineRegName}" -d "${imgDir}/${folderForDeffields}/DefField.mha" -b
+
+  name="${filename%.*}"
+  extension="${filename##*.}"
+
+  python code/ApplyTransformationsToFiles.py -i "${gtv}" -o "${imgDir}/${folderForDeffields}/${name}Rigid.${extension}" -r "${imgDir}/${affineRegName}" -b
+
+  python gtvStats.py -f "${imgDir}/${folderForDeffields}/${name}Rigid.${extension}" -s "${imgDir}/${folderForDeffields}/${filename}" -o "gtvStats${folderForDeffields}TUM.csv"
+
+  echo "done"
 done
